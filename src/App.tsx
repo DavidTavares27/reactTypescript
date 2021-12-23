8e476b994a96b6d01d3c383462c3e2e7bd7022e4
David Tavares <davidtavares27@yahoo.com>
2021-12-23 13:54:32 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { RepositoryList } from './components/repositoryList';
+import { Counter } from './counter';
+
+export function App() {
+  return (
+    <>
+      < RepositoryList />
+      < Counter />
+    </>
+  )
+}
+
+export default App;
